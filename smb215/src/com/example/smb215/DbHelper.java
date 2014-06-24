b24a0004ad5cdcd358d68c8354cfb2a8eb9203bb
ChVivo <ChVivo@ChVivo-PC>
2014-06-24 15:39:05 +0300

    Create 2 tables

---
+++ smb215/src/com/example/smb215/DbHelper.java
@@ -0,0 +1,25 @@
+package com.example.smb215;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public  class DbHelper extends SQLiteOpenHelper {
+    
+    private static final String DATABASE_NAME = "instachat.db";
+    private static final int DATABASE_VERSION = 1;
+ 
+    public DbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+ 
+    
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table messages (_id integer primary key autoincrement, msg text, email text, email2 text, at datetime default current_timestamp);");
+        db.execSQL("create table profile (_id integer primary key autoincrement, name text, email text unique, count integer default 0);");
+    }
+ 
+ 
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    }
+}
