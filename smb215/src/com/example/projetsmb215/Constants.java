b3dcff75bd21563d2e291f7a15ee8eefe6c6fd5e
ChVivo <ChVivo@ChVivo-PC>
2014-06-26 13:14:38 +0300

    Create constant java

---
+++ smb215/src/com/example/projetsmb215/Constants.java
@@ -0,0 +1,19 @@
+package com.example.projetsmb215;
+
+public interface Constants {
+	 
+    /**
+     * Base URL of the Demo Server (such as http://my_host:8080/gcm-demo)
+     */
+    String SERVER_URL = "http://localhost:8080/GcmServer/";
+ 
+    /**
+     * Google API project id registered to use GCM.
+     */
+    String SENDER_ID = "1086298744334";
+    static final String TAG="GCMTesting";
+    static final String DISPLAY_MESSAGE_ACTION =
+            "com.example.projetsmb215.DISPLAY_MESSAGE";
+ 
+    static final String EXTRA_MESSAGE = "message";
+}
